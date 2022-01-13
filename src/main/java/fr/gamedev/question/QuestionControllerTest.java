a0a3b3a6757e7af291735e4e64141cea6e795b2b
swan <swan@argelium.fr>
2022-01-13 14:43:07 +0100

    add QuestionControllerTest file + lib

---
+++ src/main/java/fr/gamedev/question/QuestionControllerTest.java
@@ -0,0 +1,14 @@
+package fr.gamedev.question;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionControllerTest {
+
+    @Test
+    void answer() {
+        Assertions.assertTrue(true);
+    }
+}
