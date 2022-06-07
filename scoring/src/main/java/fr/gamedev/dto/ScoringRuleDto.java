ba56a1161b188f12a29abe22bf47b53ea7e423f4
Swan BENOIT <swan.benoit@gmail.com>
2022-06-07 18:13:31 +0200

    rollback to java 11 for consistency

--- scoring/src/main/java/fr/gamedev/dto/ScoringRuleDto.java
+++ scoring/src/main/java/fr/gamedev/dto/ScoringRuleDto.java
@@ -1,3 +1,36 @@
 package fr.gamedev.dto;
 
-public record ScoringRuleDto(long questionId, int nbPoint){ }
+import java.util.Objects;
+
+public final class ScoringRuleDto {
+    private final long questionId;
+    private final int nbPoint;
+
+    ScoringRuleDto(long questionId, int nbPoint) {
+        this.questionId = questionId;
+        this.nbPoint = nbPoint;
+    }
+
+    public long questionId() {
+        return questionId;
+    }
+
+    public int nbPoint() {
+        return nbPoint;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ScoringRuleDto) obj;
+        return this.questionId == that.questionId &&
+                this.nbPoint == that.nbPoint;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(questionId, nbPoint);
+    }
+
+}
--- scoring/src/main/java/fr/gamedev/dto/UserAnswerDTO.java
+++ scoring/src/main/java/fr/gamedev/dto/UserAnswerDTO.java
@@ -1,3 +1,43 @@
 package fr.gamedev.dto;
 
-public record UserAnswerDTO(long questionId, long userId, String response) {}
+import java.util.Objects;
+
+public final class UserAnswerDTO {
+    private final long questionId;
+    private final long userId;
+    private final String response;
+
+    UserAnswerDTO(long questionId, long userId, String response) {
+        this.questionId = questionId;
+        this.userId = userId;
+        this.response = response;
+    }
+
+    public long questionId() {
+        return questionId;
+    }
+
+    public long userId() {
+        return userId;
+    }
+
+    public String response() {
+        return response;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (UserAnswerDTO) obj;
+        return this.questionId == that.questionId &&
+                this.userId == that.userId &&
+                Objects.equals(this.response, that.response);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(questionId, userId, response);
+    }
+
+}
